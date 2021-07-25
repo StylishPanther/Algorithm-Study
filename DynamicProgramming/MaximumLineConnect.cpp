7a5019e47e9bb948d04b185e0ad5909142adbd66
leezino09 <44574410+StylishPanther@users.noreply.github.com>
2021-07-25 22:38:54 +0900

    최대 선 연결하기 (동적 계획법)

---
+++ DynamicProgramming/MaximumLineConnect.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() 
+{
+	int n;
+	cin >> n;
+	vector<int> arr(n+1);
+	vector<int> dy(n+1);
+	
+	int res = 0;
+	for (int i = 1; i<=n; i++)
+	{
+		cin >> arr[i];
+	}
+	dy[1] = 1;
+	for(int i = 2; i<=n; i++)
+	{
+		int max = 0;
+		for(int j = i-1; j>=0 ; j--)
+		{
+			if(arr[j] < arr[i] && dy[j] > max) max = dy[j];
+		}
+		
+		dy[i] = max + 1;
+		if(res < dy[i]) res = dy[i];
+	}
+	
+	cout << res;
+	
+}
+
+#endif
